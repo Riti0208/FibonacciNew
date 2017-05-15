7cc6ea9eaaf65a437c4a98fd93a24a63536ef628
くらくら <daichizaki@yahoo.co.jp>
2017-05-15 21:45:31 +0900

    Commit

---
+++ fibonacciNew.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace fibonacci{
+    
+    class MainClass{
+        static int[] fibonacciList;
+        public static void Main(){
+            fibonacciList = new int[100];
+            for (int i = 0; i < 100; i++){
+                fibonacciList[i] = fibonacci(i);
+                Console.WriteLine("n : " + i + " = " + fibonacciList[i]);
+            }
+        }
+        private static int fibonacci(int value){
+			if (value == 0) return 0;
+			if (value == 1) return 1;
+            return fibonacciList[value - 2] + fibonacciList[value - 1];
+        }
+
+    }
+}
